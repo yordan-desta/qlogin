2a8cd6e6a8638e18948c6099e095594fb471a991
pyordan <me@gmail.com>
2015-04-07 23:53:27 -0700

    ldap login done

--- __init__.py
+++ __init__.py
@@ -3,7 +3,9 @@ import LoginFormui
 import sys
 from PyQt4.QtCore import  *
 from PyQt4.QtGui import *
+import ldap
 
+server="LDAP://172.20.0.71"
 user="root"
 pwd="pass"
 authenticated= False
@@ -13,8 +15,6 @@ statusText="<html><head/><body><p><span style=\" font-style:italic; color:#ff2b0
 
 def sendToprinter(username,password):
     file=open("../logfile","w+")
-    
-    #file.write("&*********************&&&**%#$")
 
     print username,":",password
 
@@ -38,17 +38,33 @@ def sendToprinter(username,password):
         print "authenticated"
         file.write(writeText)
         subprocess.call(openFile)
-        file.write("^^&$%&*(YHHYGYUVF(&&*)()(UIHG^FYUGYU^S%^&TUTYTYTRTWJHGYGGI#*")
+        file.write("")
         file.close()
         app.quit()
 
-
+def ldapConLogin(username,password):
+    con = ldap.initialize(server)
+    who = str(username)
+    cred = str(password)
+    try:
+        con.simple_bind(who, cred)
+        print "authenticated user"
+    except ldap.INVALID_CREDENTIALS:
+        print "invalid credentials"
+    except ldap.LDAPError, e:
+        if type(e.message) == dict and e.message.has_key('desc'):
+            print e.message['desc']
+        else:
+            print e
+        print "can't connect to the server"
+        sys.exit()
 
 class Form(QDialog,LoginFormui.Ui_loginFormMain):
     def __init__(self,parent=None):
         super(Form,self).__init__(parent)
 
         self.setupUi(self)
+
 
         self.connect(self.login_button,SIGNAL("clicked()"),self.logInfunc)
         self.connect(self.cancel_button,SIGNAL("clicked()"),app.quit)
@@ -57,6 +73,7 @@ class Form(QDialog,LoginFormui.Ui_loginFormMain):
         print "Logged in by:user ",self.username.text(),":\n","password: ",self.password.text()
 
         sendToprinter(self.username.text(),self.password.text())
+        ldapConLogin(self.username.text(),self.password.text())
 
 app=QApplication(sys.argv)
 form=Form()
